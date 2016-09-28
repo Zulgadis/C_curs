626beaafc804546de434c6f0ccde78c4e5d84b95
Zulgadis <limp.iq@gmail.com>
2016-09-28 13:04:31 -0400

    master branch testing

--- First_app_1_1/First_app_1_1/Program.cs
+++ First_app_1_1/First_app_1_1/Program.cs
@@ -10,7 +10,7 @@ namespace First_app_1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Master");
         }
     }
 }
