074363888f6734647f4cc0b59b553b9c09937fcb
Zulgadis <limp.iq@gmail.com>
2016-09-28 14:40:55 -0400

    New data

--- First_app_1_1/First_app_1_1/Program.cs
+++ First_app_1_1/First_app_1_1/Program.cs
@@ -10,8 +10,39 @@ namespace First_app_1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Second");
-            Console.WriteLine("4+1=" + (5+7));
+           /* Console.WriteLine("Please enter KM");
+            int kmMeters;
+            kmMeters = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(kmMeters + "km to meters = " + kmMeters * 1000 + "m");
+
+            Console.WriteLine("Please enter OST");
+            int ost;
+            ost = int.Parse(Console.ReadLine());
+            Console.WriteLine(ost + " % 2 = " + ost % 2);
+
+
+            Console.WriteLine("Please enter number");
+            int number;
+            number = int.Parse(Console.ReadLine());
+
+           
+            Console.WriteLine(number + " " + number / 1000 + " " + number / 100%10 + " " + number / 10%10 + " " + number % 10);
+
+    */
+            Console.WriteLine("Enter km Number");
+            int km;
+            km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter l/1km");
+            int lit;
+            lit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(lit * km + " lir we spend per " + km + " km");
+
+
+          
+
         }
     }
 }
