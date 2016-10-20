c8c2435cc2189a3478d59f5a5f334bb5bb9617ed
Zulgadis <limp.iq@gmail.com>
2016-10-20 04:42:07 -0400

    Lesson4

--- Home_work3/Home_work3/Program.cs
+++ Home_work3/Home_work3/Program.cs
@@ -11,13 +11,13 @@ namespace Home_work3
         static void Main(string[] args)
         {
             //Вариант 1
-            /*
+           /*
             Console.WriteLine("Please enter distans: ");
             int dis = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter time of the day:\na)\tday\nb)\tnight");
             string time = Console.ReadLine();
             float money = dis;
-            switch (time)
+            switch (time.ToLower())
             {
                 case "day":
                 case "a":
@@ -39,26 +39,28 @@ namespace Home_work3
                     break;
             }
             money = money + 30;
-            Console.WriteLine($"The {dis} km in {time} will be coast {money:0.00} ");
-            */
+            Console.WriteLine($"The {dis} km in {time} will be coast {money:0} ");
+         */
 
             //Вариант 2
             Console.WriteLine("Please enter distans: ");
               int dis = int.Parse(Console.ReadLine());
               Console.WriteLine("Please enter time of the day:\na)\tday\nb)\tnight");
-              string time = Console.ReadLine();
-              float money = dis;
+              string time = Console.ReadLine().ToLower();
+            
+            float money = dis;
               if (dis <= 15)
                   money = money * 5;
               else if (dis > 15 && dis <= 30)
                   money = money * 4;
               else
                   money = money * 2;
-              if (time == "night")
+            
+            if (time == "night" || time == "b")
                   money = money / 2;    
               money = money + 30;
               Console.WriteLine($"The {dis} km in {time} will be coast {money} ");
-              
+             
         }
     }
 }
---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] par = new int[6];
+            for (int i = 0; i < par.Length; i++)
+            {
+                Console.WriteLine("Enter Number");
+                par[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Numbers:");
+            for (int i = 0; i < par.Length; i++)
+            {
+                if (par[i] % 2 == 0)
+                    Console.WriteLine(par[i]);
+            }
+            
+            //////////////////
+            Console.WriteLine("Enter number:");
+            
+            int i;
+            for (i = 0; i < num; i++)
+            {
+               
+            }
+            
+
+            ///////////////////////////
+            int sum = 0;
+            int num2 = 0;
+            Console.WriteLine("Enter Numbers");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 10 ; i < num+10; i++)
+            {
+                num2 = int.Parse(Console.ReadLine());
+                if (num2 % 5 == 0)
+                sum += num2;
+            }
+            Console.WriteLine("Sum: {0}", sum);
+           
+            /////////////
+            Console.WriteLine("Number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Step:");
+            int step = int.Parse(Console.ReadLine());
+            int num1 = 1;
+            for (int i = 0; i < step; i++)
+            {
+                num1 = num * num1;
+            }
+            Console.WriteLine("Total: {0}", num1);
+            */
+            /////////////// While
+        }
+
+    }
+}
